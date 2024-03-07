35d4b1af57d316377ba845faf0d785c90819c821
Suvam Chakraborti <nfgaming4523@gmail.com>
2024-03-07 21:46:16 +0530

    Page2 & page4 Done

--- script.js
+++ script.js
@@ -36,15 +36,15 @@ function loader(){
       duration: 0.6,
       ease: Power4,
     });
+    tl.to("#loader", {
+        display: "none",
+    });
     tl.from("#nav",{
         opacity:0
     })
-    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
-        y:150,
-        stagger:0.2
-    })
-    tl.to("#loader", {
-      display: "none",
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+      y: 150,
+      stagger: 0.2
     });
 }
 function cursorAnimaton(){
